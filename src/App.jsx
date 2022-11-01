e9c390d5c9f8d0c3b2a72aee3371626de9c2251e
Porasjeet Singh <porasjeet@gmail.com>
2022-11-01 11:10:21 +0530

    Add controls

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Hyperbeam from "@hyperbeam/web";
+import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { useEffect, useRef, useState } from "react";
 import * as THREE from "three";
@@ -8,6 +9,7 @@ function App() {
   const hyperbeamContainerRef = useRef(null);
   const [texture, setTexture] = useState(new THREE.Texture());
   const [hyperbeam, setHyperbeam] = useState(null);
+  const [isControlsEnabled, setIsControlsEnabled] = useState(true);
 
   useEffect(() => {
     (async () => {
@@ -45,7 +47,47 @@ function App() {
       <div className="hyperbeam-container" ref={hyperbeamContainerRef}></div>
       <div className="canvas-container">
         <Canvas>
-          <mesh position={[0, 0, 0]} rotation={[0, Math.PI, Math.PI]}>
+          <mesh
+            position={[0, 0, 0]}
+            rotation={[0, Math.PI, Math.PI]}
+            onPointerEnter={() => setIsControlsEnabled(false)}
+            onPointerLeave={() => setIsControlsEnabled(true)}
+            onPointerDown={(e) => {
+              if (hyperbeam) {
+                hyperbeam.sendEvent({
+                  type: "mousedown",
+                  x: e.intersections[0].uv.x,
+                  y: e.intersections[0].uv.y,
+                });
+              }
+            }}
+            onPointerMove={(e) => {
+              if (hyperbeam) {
+                hyperbeam.sendEvent({
+                  type: "mousemove",
+                  x: e.intersections[0].uv.x,
+                  y: e.intersections[0].uv.y,
+                });
+              }
+            }}
+            onPointerUp={(e) => {
+              if (hyperbeam) {
+                hyperbeam.sendEvent({
+                  type: "mouseup",
+                  x: e.intersections[0].uv.x,
+                  y: e.intersections[0].uv.y,
+                });
+              }
+            }}
+            onWheel={(e) => {
+              if (hyperbeam) {
+                hyperbeam.sendEvent({
+                  type: "wheel",
+                  deltaY: e.deltaY,
+                });
+              }
+            }}
+          >
             <planeGeometry attach="geometry" args={[10, (9 / 16) * 10]} />
             <meshBasicMaterial
               attach="material"
@@ -53,6 +95,7 @@ function App() {
               side={THREE.DoubleSide}
             />
           </mesh>
+          <OrbitControls makeDefault enabled={isControlsEnabled} />
         </Canvas>
       </div>
     </div>
