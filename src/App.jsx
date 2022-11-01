c213cf1368c8556090a7bc69aa297edb38988eb0
Porasjeet Singh <porasjeet@gmail.com>
2022-11-01 09:26:01 +0530

    Create a scene w/ Hyperbeam

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import Hyperbeam from "@hyperbeam/web";
+import { Canvas } from "@react-three/fiber";
+import { useEffect, useRef, useState } from "react";
+import * as THREE from "three";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const hyperbeamContainerRef = useRef(null);
+  const [texture, setTexture] = useState(new THREE.Texture());
+  const [hyperbeam, setHyperbeam] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      const room = location.pathname.substring(1);
+      const response = await fetch(
+        `https://demo-api.tutturu.workers.dev/${room}`
+      );
+      if (response.status >= 400) {
+        alert(
+          "We are out of demo servers! Visit hyperbeam.dev to get your own API key."
+        );
+        return;
+      }
+      const body = await response.json();
+      if (room !== body.room) {
+        history.pushState(null, null, `/${body.room}${location.search}`);
+      }
+      setHyperbeam(
+        await Hyperbeam(hyperbeamContainerRef.current, body.url, {
+          frameCb: (frame) => {
+            const texture = new THREE.Texture();
+            texture.flipY = false;
+            texture.generateMipmaps = false;
+            texture.image = frame;
+            texture.needsUpdate = true;
+            setTexture(texture);
+          },
+        })
+      );
+    })();
+  }, []);
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="hyperbeam-container" ref={hyperbeamContainerRef}></div>
+      <div className="canvas-container">
+        <Canvas>
+          <mesh position={[0, 0, 0]} rotation={[0, Math.PI, Math.PI]}>
+            <planeGeometry attach="geometry" args={[10, (9 / 16) * 10]} />
+            <meshBasicMaterial
+              attach="material"
+              map={texture}
+              side={THREE.DoubleSide}
+            />
+          </mesh>
+        </Canvas>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
